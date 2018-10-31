15fbf5645aa09222480d7780d9030464a6116819
AlexCipriano <akatsukimember5550@hotmail.com>
2018-10-30 23:15:04 -0400

    First Clean Push
    First clean push of Town Builder Game

---
+++ Town Builder/Assets/scripts/PlaceBuilding.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceBuilding : MonoBehaviour{
+
+	public Grid grid;
+	public GameObject prefab;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hitinfo;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			if (Physics.Raycast (ray, out hitinfo)) {
+				PlaceBuildingNear (hitinfo.point);
+			}
+		}
+	}
+
+
+	private void PlaceBuildingNear(Vector3 near){
+		var finalPosition = grid.GetGridPoint(near);
+		finalPosition += new Vector3 (0f, 2.5f, 0f);
+		Instantiate (prefab, finalPosition, Quaternion.identity);
+	}
+
+}
